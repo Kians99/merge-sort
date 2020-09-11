08ec28e92ebc32783f5f440d8ccd117081c2cce1
Kian <Ksadeghi@sas.upenn.edu>
2020-09-11 12:44:03 -0400

    Added tests for merge sort

--- merge-sort.rb
+++ merge-sort.rb
@@ -1,35 +1,45 @@
   def merge(left_arr, right_arr)
     new_array = []
     until right_arr == [] || left_arr == []
-      if right_arr[0] >= left_arr[0]
-        new_array.push(left_arr.shift)
-      else
-        new_array.push(right_arr.shift)
-      end
+      right_arr[0] >= left_arr[0] ? new_array.push(left_arr.shift) : new_array.push(right_arr.shift)
     end
-    left_arr == [] && right_arr == [] ? (return new_array) : (left_arr == [] ? (new_array.push(right_arr)) : (new_array.push(left_arr)))
-    return new_array.flatten
+    return new_array if left_arr == [] && right_arr == []
+
+    left_arr == [] ? new_array.push(right_arr) : new_array.push(left_arr)
+    new_array.flatten
   end
-    
+
   def merge_sort(array)
     if array.length <= 1
-      return array
+      array
     else
       mid_point = (array.length - 1) / 2
       left_arr = merge_sort(array[0..mid_point])
-      right_arr = merge_sort(array[mid_point+1..(array.length - 1)])
+      right_arr = merge_sort(array[mid_point + 1..(array.length - 1)])
       merge(left_arr, right_arr)
     end
   end
-  
+
   def construct_random
     arr = []
     10.times { arr.push(rand(100)) }
     arr
   end
-    
+
   def array_of_arrays
     arr = []
     10.times { arr.push(construct_random) }
     arr
   end
+
+  arr = array_of_arrays
+  arr.each do |array|
+    puts "Input: "
+    p array
+    puts "Output: "
+    p merge_sort(array)
+    puts ""
+  end
+
+  
+    
